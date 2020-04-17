57d904280e047449f020fc476cf75d7a822956d0
Fred Labanda <coderoph@gmail.com>
2020-04-17 22:50:11 +0800

    Init es6 module

---
+++ src/index.js
@@ -0,0 +1,16 @@
+module.exports = function pascalize(object) {
+    const newObject = {};
+    if (object) {
+        for (const [key, value] of Object.entries(object)) {
+            const newKey = key.replace(/^\w/, item => item.toUpperCase());
+
+            if (typeof value === 'object') {
+                newObject[newKey] = pascalize(value);
+            } else {
+                newObject[newKey] = value;
+            }
+        }
+    }
+
+    return newObject;
+}
